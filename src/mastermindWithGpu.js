5a1908ebe0df2ad06e358641aea94070be4ee935
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-06-28 11:07:29 +0100

    Tweaks

--- src/mastermindWithGpu.js
+++ src/mastermindWithGpu.js
@@ -22,23 +22,25 @@ import {
 // }
 
 const calculateNewGuess = untried => {
-  const best = ALL_CODES.reduce((currentBest, unusedCode) => {
-    const max = ALL_SCORES.reduce((currentMax, score) => {
-      const count = countWithPredicate(untried, evaluatesToSameScore(unusedCode, score))
+  const initialBest = { count: Number.MAX_VALUE }
+  const best = ALL_CODES.reduce((currentBest, allCode) => {
+    const maxCount = ALL_SCORES.reduce((currentMax, allScore) => {
+      const count = countWithPredicate(untried, evaluatesToSameScore(allCode, allScore))
       return Math.max(currentMax, count)
     }, 0)
-    return max < currentBest.min ? { min: max, guess: unusedCode } : currentBest
-  }, { min: Number.MAX_VALUE })
+    return maxCount < currentBest.count
+      ? { count: maxCount, guess: allCode }
+      : currentBest
+  }, initialBest)
   return best.guess
 }
 
-const solve = (attempt, untried, history) => {
-  console.log(`[mastermindWithoutGpu] untried length: ${untried.length}`)
+const solve = (attempt, untried = ALL_CODES, history = []) => {
+  console.log(`[mastermindWithGpu solve] untried length: ${untried.length}`)
   const guess = history.length === 0 ? INITIAL_GUESS :
     untried.length === 1 ? untried[0] : calculateNewGuess(untried)
-  console.log(`[mastermindWithoutGpu] generated guess: ${codeToString(guess)}`)
   const score = attempt(guess)
-  console.log(`[mastermindWithoutGpu] score: ${scoreToString(score)}`)
+  console.log(`[mastermindWithGpu solve] guess: ${codeToString(guess)}; score: ${scoreToString(score)}`)
   const newHistory = [...history, { guess, score }]
   if (score.blacks === 4) return newHistory
   const newUntried = untried.filter(evaluatesToSameScore(guess, score))
@@ -46,7 +48,8 @@ const solve = (attempt, untried, history) => {
 }
 
 export const mastermindWithGpu = secret => {
-  console.log(`[mastermindWithoutGpu] secret: ${codeToString(secret)}`)
-  const history = solve(guess => evaluateScore(secret, guess), ALL_CODES, [])
-  console.log(`[mastermindWithoutGpu] numAttempts: ${history.length}`)
+  console.log(`[mastermindWithGpu] secret: ${codeToString(secret)}`)
+  const attempt = guess => evaluateScore(secret, guess)
+  const history = solve(attempt)
+  console.log(`[mastermindWithGpu] numAttempts: ${history.length}`)
 }
