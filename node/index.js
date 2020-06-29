b4b8e0e8f976f220a2bca0260acac2414b8f3a73
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-06-29 19:14:58 +0100

    Use gpu.addFunction for kernel helper functions

--- node/index.js
+++ node/index.js
@@ -33,7 +33,7 @@ function codeToString(code) {
   return `${p0}-${p1}-${p2}-${p3}`
 }
 
-function allCodeFromIndex(index) {
+function allCodeFromIndex(allPegs, index) {
   const p0 = allPegs[Math.trunc(index / 216) % 6]
   const p1 = allPegs[Math.trunc(index / 36) % 6]
   const p2 = allPegs[Math.trunc(index / 6) % 6]
@@ -42,11 +42,10 @@ function allCodeFromIndex(index) {
 }
 
 function encodeCode(code) {
-  const [a, b, c, d] = code
-  const p0 = a
-  const p1 = b << 4
-  const p2 = c << 8
-  const p3 = d << 12
+  const p0 = code[0]
+  const p1 = code[1] << 4
+  const p2 = code[2] << 8
+  const p3 = code[3] << 12
   return p0 | p1 | p2 | p3
 }
 
@@ -58,39 +57,26 @@ function decodeCode(encoded) {
   return [p0, p1, p2, p3]
 }
 
-const fred = gpu.createKernel(function (allPegs) {
+gpu.addFunction(allCodeFromIndex)
+gpu.addFunction(encodeCode)
+gpu.addFunction(decodeCode)
+// gpu.addFunction(evaluateScore)
+// gpu.addFunction(findBest)
 
-  function allCodeFromIndex(allPegs, index) {
-    return [
-      allPegs[Math.trunc(index / 216) % 6],
-      allPegs[Math.trunc(index / 36) % 6],
-      allPegs[Math.trunc(index / 6) % 6],
-      allPegs[index % 6]
-    ]
-  }
-
-  function encodeCode(code) {
-    return code[0] | (code[1] << 4) | (code[2] << 8) | (code[3] << 12)
-  }
-
-  function decodeCode(encoded) {
-    return [
-      encoded & 0x000f,
-      (encoded & 0x00f0) >> 4,
-      (encoded & 0x0f00) >> 8,
-      (encoded & 0xf000) >> 12
-    ]
-  }
-
+function kernelEntryPoint(allPegs) {
   const index = this.thread.x
   return encodeCode(allCodeFromIndex(allPegs, index))
-}).setOutput([1296])
+}
+
+const settings = {
+  output: [1296]
+}
+
+const kernel = gpu.createKernel(kernelEntryPoint, settings)
 
 const main = () => {
-  const code = allCodeFromIndex(1295)
-  console.log(`code: ${codeToString(code)}`)
-  const fredResult = fred(allPegs)
-  fredResult.forEach(code => console.log(codeToString(decodeCode(code))))
+  const results = kernel(allPegs)
+  results.forEach(code => console.log(codeToString(decodeCode(code))))
 }
 
 main()
